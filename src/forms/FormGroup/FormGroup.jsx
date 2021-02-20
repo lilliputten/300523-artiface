305bd7098b79783202ccfe4c59c323165983ee06
lilliputten <lilliputten@yandex.ru>
2021-02-20 17:31:21 +0300

    FormGroups: restructured styles, added mod props (`separated` (instead of `tagged`), `shaded`, `padded`).

--- src/forms/FormGroup/FormGroup.jsx
+++ src/forms/FormGroup/FormGroup.jsx
@@ -1,30 +1,32 @@
 /** @module FormGroup
  *  @class FormGroup
  *  @since 2020.07.20, 19:07
- *  @changed 2021.02.20, 13:45
+ *  @changed 2021.02.20, 17:30
  */
 /* eslint-disable react/require-default-props, react/no-unused-prop-types */
 
 import React from 'react';
 import PropTypes from 'prop-types';
-// import connect from 'react-redux/es/connect/connect'
 import { cn } from 'utils/configure';
 
 import FormItemHOC from '../FormItemHOC';
 import FormSeparator from '../FormSeparator';
+import FormContainer from '../FormContainer';
 
 import './FormGroup.pcss';
+import './FormGroup-Layout.pcss';
+import './FormGroup-Variations.pcss';
 
 const cnFormGroup = cn('FormGroup');
 
 class FormGroup extends React.PureComponent /** @lends @FormGroup.prototype */ {
 
   static propTypes = {
-    // content: PropTypes.any, // ???
+    // background: PropTypes.string,
     align: PropTypes.string,
     alignItems: PropTypes.string,
-    background: PropTypes.string,
     className: PropTypes.string,
+    content: PropTypes.element, // ???
     disabled: PropTypes.bool, // ???
     flow: PropTypes.bool,
     fullWidth: PropTypes.bool,
@@ -32,6 +34,9 @@ class FormGroup extends React.PureComponent /** @lends @FormGroup.prototype */ {
     noSpace: PropTypes.bool,
     noWrap: PropTypes.bool,
     padded: PropTypes.bool,
+    separated: PropTypes.bool,
+    shaded: PropTypes.bool,
+    spaced: PropTypes.bool,
     stack: PropTypes.bool,
     wrap: PropTypes.bool,
   }
@@ -42,15 +47,17 @@ class FormGroup extends React.PureComponent /** @lends @FormGroup.prototype */ {
     // Style-related modifiers...
     'align',
     'alignItems',
-    'background',
+    // 'background',
     'flow',
     'fullWidth',
     'noSpace',
     'noWrap',
     'padded',
+    'separated',
+    'shaded',
+    'spaced',
     'stack',
     'wrap',
-    'separated',
     // 'disabled',
   ]
 
@@ -81,16 +88,27 @@ class FormGroup extends React.PureComponent /** @lends @FormGroup.prototype */ {
       children,
       content,
       separated,
+      container,
     } = this.props;
 
     // const renderProps = this.getRenderProps()
 
-    return (
+    let result = (
       <div id={id} className={this.getClassName()}>
         {separated && (<FormSeparator />)}
         {content || children}
       </div>
     );
+
+    if (container) {
+      result = (
+        <FormContainer>
+          {result}
+        </FormContainer>
+      );
+    }
+
+    return result;
   }
 
 }
